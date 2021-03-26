8f3dc0e71c8f932d310e683191e3a83521298e39
Chenxu <chenxu4012@foxmail.com>
2021-03-26 15:13:33 +0800

    修改设备字段

---
+++ src/service/user.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: Chenxu
+ * @Date: 2021-03-24 10:23:05
+ * @LastEditTime: 2021-03-26 00:55:40
+ * @Msg: Nothing
+ */
+import { BaseService, Service, Permission } from "cl-admin";
+
+@Service("base/sys/user")
+class User extends BaseService { }
+
+export default User;
